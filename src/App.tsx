541abb4db1caaacb16c81411f03e5c33b0973818
soft-coded <shrutanten.work@gmail.com>
2022-09-10 02:39:57 +0530

    Add folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <main className="app-root dynamic-full-screen">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
